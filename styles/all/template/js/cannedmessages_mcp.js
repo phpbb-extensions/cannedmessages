d244740b52710d7b4947021c0c70353d9b41710b
Matt Friedman <maf675@gmail.com>
2018-01-05 11:56:57 -0800

    Refactor JavaScript a little bit

--- styles/all/template/js/cannedmessages_mcp.js
+++ styles/all/template/js/cannedmessages_mcp.js
@@ -31,36 +31,23 @@ text_name = 'cannedmessage_content';
 	});
 
 	$(function() {
+        var $add_edit = $('#cannedmessage_add_edit'),
+			$content = $('#cannedmessage_content_section'),
+			$preview = $('#cannedmessage_preview'),
+			$preview_btn = $('#preview'),
+			$cancel_btn = $('#cancel'),
+			$action_cancel = $('#action_cancel');
 
-        $('#is_cat1').on('change', function() {
-           toggleContent($(this).is(':checked'));
+		$('#is_cat1, #is_cat0').on('change', function() {
+			$content.add($preview).add($preview_btn).toggle($(this).is(':checked') && $(this).attr('id') === 'is_cat0');
+		});
+
+		$preview_btn.on('click', function() {
+            $add_edit.prop('action', $add_edit.prop('action') + '#cannedmessage_preview');
         });
 
-        $('#is_cat0').on('change', function() {
-            toggleContent(!$(this).is(':checked'));
+		$cancel_btn.on('click', function() {
+            $add_edit.prop('action', $action_cancel.val());
         });
-
-        $('#preview').on('click', function() {
-            var action = $('#cannedmessage_add_edit').prop('action');
-
-            $('#cannedmessage_add_edit').prop('action', action + '#cannedmessage_preview');
-        });
-
-        $('#cancel').on('click', function() {
-            $('#cannedmessage_add_edit').prop('action', $('#action_cancel').val());
-        });
-
-        function toggleContent(isChecked) {
-            if (isChecked) {
-                $('#cannedmessage_content_section').hide();
-                $('#preview').hide();
-                $('#cannedmessage_preview').hide();
-            }
-            else {
-                $('#cannedmessage_content_section').show();
-                $('#preview').show();
-                $('#cannedmessage_preview').show();
-            }
-        }
     });
 })(jQuery); // Avoid conflicts with other libraries

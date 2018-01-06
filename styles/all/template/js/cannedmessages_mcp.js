cf083009251303a5f4cb4c0c37367ba4a8f227ae
Matt Friedman <maf675@gmail.com>
2018-01-06 02:00:29 -0800

    Fix whitespaces

--- styles/all/template/js/cannedmessages_mcp.js
+++ styles/all/template/js/cannedmessages_mcp.js
@@ -2,28 +2,28 @@ form_name = 'cannedmessage_add_edit';
 text_name = 'cannedmessage_content';
 
 (function($) { // Avoid conflicts with other libraries
-    'use strict';
-    phpbb.addAjaxCallback('row_down', function(res) {
-        if (typeof res.success === 'undefined' || !res.success) {
-            return;
-        }
+	'use strict';
+	phpbb.addAjaxCallback('row_down', function(res) {
+		if (typeof res.success === 'undefined' || !res.success) {
+			return;
+		}
 
-        var $firstLi = $(this).parents('li'),
-            $secondLi = $firstLi.next();
+		var $firstLi = $(this).parents('li'),
+			$secondLi = $firstLi.next();
 
-        $firstLi.insertAfter($secondLi);
-    });
+		$firstLi.insertAfter($secondLi);
+	});
 
-    phpbb.addAjaxCallback('row_up', function(res) {
-        if (typeof res.success === 'undefined' || !res.success) {
-            return;
-        }
+	phpbb.addAjaxCallback('row_up', function(res) {
+		if (typeof res.success === 'undefined' || !res.success) {
+			return;
+		}
 
-        var $secondLi = $(this).parents('li'),
-            $firstLi = $secondLi.prev();
+		var $secondLi = $(this).parents('li'),
+			$firstLi = $secondLi.prev();
 
-        $secondLi.insertBefore($firstLi);
-    });
+		$secondLi.insertBefore($firstLi);
+	});
 
 	// This removes the parent row of the link or form that fired the callback.
 	phpbb.addAjaxCallback('row_delete', function() {
@@ -31,7 +31,7 @@ text_name = 'cannedmessage_content';
 	});
 
 	$(function() {
-        var $add_edit = $('#cannedmessage_add_edit'),
+		var $add_edit = $('#cannedmessage_add_edit'),
 			$content = $('#cannedmessage_content_section'),
 			$preview = $('#cannedmessage_preview'),
 			$preview_btn = $('#preview'),
@@ -43,11 +43,11 @@ text_name = 'cannedmessage_content';
 		});
 
 		$preview_btn.on('click', function() {
-            $add_edit.prop('action', $add_edit.prop('action') + '#cannedmessage_preview');
-        });
+			$add_edit.prop('action', $add_edit.prop('action') + '#cannedmessage_preview');
+		});
 
 		$cancel_btn.on('click', function() {
-            $add_edit.prop('action', $action_cancel.val());
-        });
-    });
+			$add_edit.prop('action', $action_cancel.val());
+		});
+	});
 })(jQuery); // Avoid conflicts with other libraries
--- styles/all/template/js/cannedmessages_posting.js
+++ styles/all/template/js/cannedmessages_posting.js
@@ -1,15 +1,15 @@
 (function($, u_cannedmessage_selected) {
-    'use strict';
-    $(function() {
-        $('#cannedmessages').on('change', function() {
-            var cannedmessage_id = $(this).val();
-            if (cannedmessage_id !== '') {
-                $.get(u_cannedmessage_selected.replace(/0$/, cannedmessage_id), function(message_contents) {
-                    if (message_contents !== '') {
-                        insert_text(message_contents, false);
-                    }
-                });
-            }
-        });
-    });
+	'use strict';
+	$(function() {
+		$('#cannedmessages').on('change', function() {
+			var cannedmessage_id = $(this).val();
+			if (cannedmessage_id !== '') {
+				$.get(u_cannedmessage_selected.replace(/0$/, cannedmessage_id), function(message_contents) {
+					if (message_contents !== '') {
+						insert_text(message_contents, false);
+					}
+				});
+			}
+		});
+	});
 })(jQuery, u_cannedmessage_selected);
